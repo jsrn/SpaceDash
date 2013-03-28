370af417c19387074dff307a9281d3bdd96e0201
James Nutt <james.srn@gmail.com>
2013-03-28 13:24:57 +0000

    key mappings

---
+++ spacedash/Constants.java
@@ -0,0 +1,17 @@
+package spacedash;
+
+public class Constants {
+
+    // Keycodes
+    // Util
+    public static final int KEY_ENTER = 10;
+    public static final int KEY_ESC = 27;
+    public static final int KEY_SPACE = 32;
+    // Directions
+    public static final int KEY_LEFT = 37;
+    public static final int KEY_UP = 38;
+    public static final int KEY_RIGHT = 39;
+    public static final int KEY_DOWN = 40;
+    // Letters
+    public static final int KEY_I = 73;
+}
