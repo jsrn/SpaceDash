b8f324d991aa21b85498f60744cf9ed70b76f1b1
James Nutt <james.srn@gmail.com>
2013-03-28 14:30:02 +0000

    added grunt class

---
+++ mobiles/Grunt.java
@@ -0,0 +1,12 @@
+package mobiles;
+
+public class Grunt extends Mobile{
+    
+    public Grunt(){
+        setGraphic("grunt.png");
+        setSize(50,50);
+        setSpeed(50);
+        
+    }
+    
+}
