4b4fdbe2f8947e6e9e8ce99ad8f754c35468b0b3
James Nutt <james.srn@gmail.com>
2013-03-28 12:25:20 +0000

    set up game window and loop

--- spacedash/SpaceDash.java
+++ spacedash/SpaceDash.java
@@ -2,11 +2,18 @@ package spacedash;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 import sun.applet.Main;
 
 public class SpaceDash {
+    
+    JFrame gameWindow;
 
     public SpaceDash() {
+        // Set up game window
+        setUpGameWindow();
+        
+        // Enter game loop
         while (true) {
             try {
                 Thread.sleep(1000);
@@ -16,6 +23,15 @@ public class SpaceDash {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+    
+    private void setUpGameWindow(){
+        gameWindow = new JFrame("SpaceDash!");
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setSize(800, 600);
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setResizable(false);
+        gameWindow.setVisible(true);
     }
 
     public static void main(String[] args) {
