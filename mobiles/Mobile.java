826d6cb1eb601cc12615f56d0ea1552e23de8b4d
James Nutt <james.srn@gmail.com>
2013-03-28 13:25:12 +0000

    classes for mobile and player

---
+++ mobiles/Mobile.java
@@ -0,0 +1,58 @@
+package mobiles;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import spacedash.SpaceDash;
+
+public class Mobile {
+    private int x,y,w,h;
+    private int speed;
+    private BufferedImage graphic;
+    
+    
+    public int getSpeed(){
+        return speed;
+    }
+    
+    public void setSpeed(int s){
+        speed = s;
+    }
+    
+    public int getX(){
+        return x - (w/2);
+    }
+    
+    public int getY(){
+        return y - (h/2);
+    }
+    
+    public void setSize(int w, int h){
+        this.w = w;
+        this.h = h;
+    }
+    
+    public void setX(int newX){
+        x = newX + (w/2);
+    }
+    
+    public void setY(int newY){
+        y = newY + (y/2);
+    }
+    
+    public BufferedImage getGraphic(){
+        return graphic;
+    }
+    
+    public void setGraphic(String file){
+        try {
+            graphic = ImageIO.read(new File("src/art/" + file));
+        } catch (IOException ex) {
+            Logger.getLogger(SpaceDash.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ mobiles/Player.java
@@ -0,0 +1,12 @@
+package mobiles;
+
+public class Player extends Mobile{
+    
+    public Player(){
+        setGraphic("player.png");
+        setSize(50,50);
+        setSpeed(50);
+        
+    }
+    
+}
